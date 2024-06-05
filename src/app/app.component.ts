33b21e775a6443fb4e6628f0e4f9114ad12cc8ff
michellekenia <keniamsoaress@gmail.com>
2024-06-05 12:19:06 -0300

    iniciar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CarrinhoComponent,CommonModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'novo-projeto';
+  title = 'Afrocódigos';
+  link = 'google.com'
 }
---
+++ src/app/carrinho/carrinho.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './carrinho.component.html',
+  styleUrl: './carrinho.component.css'
+})
+export class CarrinhoComponent {
+
+}
